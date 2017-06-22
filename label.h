ef0221ba5f4cf530973931d72628146d3c1aaf37
Quan Wang <BetterWang@gmail.com>
2017-06-22 19:27:04 +0200

    [+] update

--- label.h
+++ label.h
@@ -2,11 +2,21 @@
 char const * fname[] = {
 	"test",							// 0 test
 	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v2/170622_135650/0000/",			// 1 Ks HM0 pPb
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v2/170622_135720/0000/",			// 2 Ks HM1 pPb
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v2/170622_135740/0000/",			// 3 Ks HM7 pPb
+	"",			// 4 Ks HM0 pPb reverse
+	"",			// 5 Ks HM1 pPb reverse
+	"",			// 6 Ks HM7 pPb reverse
 };
 
 char const * ftxt[] = {
-	"txt/test/",			// 0
-	"txt/PAHM0_Ks/",		// 1
+	"txt/test/",				// 0
+	"txt/PAHM0_Ks/",			// 1
+	"txt/PAHM1_Ks/",			// 2
+	"txt/PAHM7_Ks/",			// 3
+	"txt/PAHM0_Ks_reverse/",		// 4
+	"txt/PAHM1_Ks_reverse/",		// 5
+	"txt/PAHM7_Ks_reverse/",		// 6
 };
 
 #include <TChain.h>
