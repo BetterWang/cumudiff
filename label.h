58ce655b0afb0e3130b75114ac3e1e80e6778c8c
Quan Wang <BetterWang@gmail.com>
2017-06-25 11:37:46 +0200

    [+] update

--- label.h
+++ label.h
@@ -1,12 +1,15 @@
 
 char const * fname[] = {
 	"test",							// 0 test
-	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v2/170622_135650/0000/",			// 1 Ks HM0 pPb
-	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v2/170622_135720/0000/",			// 2 Ks HM1 pPb
-	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v2/170622_135740/0000/",			// 3 Ks HM7 pPb
-	"",			// 4 Ks HM0 pPb reverse
-	"",			// 5 Ks HM1 pPb reverse
-	"",			// 6 Ks HM7 pPb reverse
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v4/170623_092335/0000/",			// 1 Ks HM0 pPb
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v4/170623_092351/0000/",			// 2 Ks HM1 pPb
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v4/170623_092407/0000/",			// 3 Ks HM7 pPb
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_reverse_v4/170624_231431/0000/",			// 4 Ks HM0 pPb reverse
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_reverse_v4/170624_231445/0000/",			// 5 Ks HM1 pPb reverse
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_reverse_v4/170624_231457/0000/",			// 6 Ks HM7 pPb reverse
+	"NA",			// 7 Ks HM0 pPb merge
+	"NA",			// 8 Ks HM1 pPb merge
+	"NA",			// 9 Ks HM7 pPb merge
 };
 
 char const * ftxt[] = {
@@ -17,6 +20,9 @@ char const * ftxt[] = {
 	"txt/PAHM0_Ks_reverse/",		// 4
 	"txt/PAHM1_Ks_reverse/",		// 5
 	"txt/PAHM7_Ks_reverse/",		// 6
+	"txt/PAHM0_Ks_merge/",			// 7
+	"txt/PAHM1_Ks_merge/",			// 8
+	"txt/PAHM7_Ks_merge/",			// 9
 };
 
 #include <TChain.h>
