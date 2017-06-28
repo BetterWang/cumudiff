e2eed6905eda2f1ace4326e26b8cda33464ec2a6
Quan Wang <BetterWang@gmail.com>
2017-06-28 08:08:30 +0200

    [+] update

--- runGet.sh
+++ runGet.sh
@@ -1,14 +1,14 @@
 
-./runGet3.sh 3 0 10  
-./runGet3.sh 3 1 10  
-./runGet3.sh 3 2 10  
-./runGet3.sh 3 3 10  
-./runGet3.sh 3 4 10  
-./runGet3.sh 3 5 10  
-./runGet3.sh 3 6 10  
-./runGet3.sh 3 7 10  
-./runGet3.sh 3 8 10  
-./runGet3.sh 3 9 10  
-./runGet3.sh 3 10 10 
+./runGet3.sh 2 0 10  
+./runGet3.sh 2 1 10  
+./runGet3.sh 2 2 10  
+./runGet3.sh 2 3 10  
+./runGet3.sh 2 4 10  
+./runGet3.sh 2 5 10  
+./runGet3.sh 2 6 10  
+./runGet3.sh 2 7 10  
+./runGet3.sh 2 8 10  
+./runGet3.sh 2 9 10  
+./runGet3.sh 2 10 10 
 
 
--- runGetE.sh
+++ runGetE.sh
@@ -1,2 +1,2 @@
 
-./runGetE3.sh 3
+./runGetE3.sh 2
