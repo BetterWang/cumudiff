ec026604a0b67a3adc8c529b7b9963317b6789d8
Quan Wang <BetterWang@gmail.com>
2017-06-29 16:53:52 +0200

    [+] label

--- label.h
+++ label.h
@@ -1,15 +1,24 @@
 
 char const * fname[] = {
 	"test",							// 0 test
-	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v4/170623_092335/0000/",			// 1 Ks HM0 pPb
-	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v4/170623_092351/0000/",			// 2 Ks HM1 pPb
-	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v4/170623_092407/0000/",			// 3 Ks HM7 pPb
-	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_reverse_v4/170624_231431/0000/",			// 4 Ks HM0 pPb reverse
-	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_reverse_v4/170624_231445/0000/",			// 5 Ks HM1 pPb reverse
-	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_reverse_v4/170624_231457/0000/",			// 6 Ks HM7 pPb reverse
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v7/170628_201614/0000/",			// 1 Ks HM0 pPb
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v7/170628_201631/0000/",			// 2 Ks HM1 pPb
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v7/170628_201647/0000/",			// 3 Ks HM7 pPb
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_reverse_v7/170628_201946/0000/",			// 4 Ks HM0 pPb reverse
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_reverse_v7/170628_202002/0000/",			// 5 Ks HM1 pPb reverse
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_reverse_v7/170628_202021/0000/",			// 6 Ks HM7 pPb reverse
 	"NA",			// 7 Ks HM0 pPb merge
 	"NA",			// 8 Ks HM1 pPb merge
 	"NA",			// 9 Ks HM7 pPb merge
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Lambda_cumu_eff_v7/170629_001710/0000/",			// 10 Lambda HM0 pPb
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Lambda_cumu_eff_v7/170629_001727/0000/",			// 11 Lambda HM1 pPb
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Lambda_cumu_eff_v7/170629_001746/0000/",			// 12 Lambda HM7 pPb
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Lambda_cumu_eff_reverse_v7/170629_001815/0000/",		// 13 Lambda HM0 pPb reverse
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Lambda_cumu_eff_reverse_v7/170629_001831/0000/",		// 14 Lambda HM1 pPb reverse
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Lambda_cumu_eff_reverse_v7/170629_001846/0000/",		// 15 Lambda HM7 pPb reverse
+	"NA",			// 16 Lambda HM0 pPb merge
+	"NA",			// 17 Lambda HM1 pPb merge
+	"NA",			// 18 Lambda HM7 pPb merge
 };
 
 char const * ftxt[] = {
@@ -23,6 +32,15 @@ char const * ftxt[] = {
 	"txt/PAHM0_Ks_merge/",			// 7
 	"txt/PAHM1_Ks_merge/",			// 8
 	"txt/PAHM7_Ks_merge/",			// 9
+	"txt/PAHM0_Lambda/",			// 10
+	"txt/PAHM1_Lambda/",			// 11
+	"txt/PAHM7_Lambda/",			// 12
+	"txt/PAHM0_Lambda_reverse/",		// 13
+	"txt/PAHM1_Lambda_reverse/",		// 14
+	"txt/PAHM7_Lambda_reverse/",		// 15
+	"txt/PAHM0_Lambda_merge/",		// 16
+	"txt/PAHM1_Lambda_merge/",		// 17
+	"txt/PAHM7_Lambda_merge/",		// 18
 };
 
 #include <TChain.h>
