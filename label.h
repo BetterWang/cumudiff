b92617cee264b7fc8e5d88312777bb51d5d59b1b
Quan Wang <BetterWang@gmail.com>
2018-01-30 18:56:25 +0100

    [+] label

--- label.h
+++ label.h
@@ -1,12 +1,12 @@
 
 char const * fname[] = {
 	"test",							// 0 test
-	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v10/180112_205040/0000/",			// 1 Ks HM0 pPb
-	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v10/180112_213558/0000/",			// 2 Ks HM1 pPb
-	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v10/180112_213814/0000/",			// 3 Ks HM7 pPb
-	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_reverse_v10/180112_205526/0000/",			// 4 Ks HM0 pPb reverse
-	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_reverse_v10/180112_214254/0000/",			// 5 Ks HM1 pPb reverse
-	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_reverse_v10/180112_214453/0000/",			// 6 Ks HM7 pPb reverse
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_v11/180125_225722/0000/",			// 1 Ks HM0 pPb
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_v11/180125_225748/0000/",			// 2 Ks HM1 pPb
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_v11/180125_230027/0000/",			// 3 Ks HM7 pPb
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_Kscumu_eff_reverse_v11/180125_230250/0000/",			// 4 Ks HM0 pPb reverse
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_Kscumu_eff_reverse_v11/180125_230315/0000/",			// 5 Ks HM1 pPb reverse
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_Kscumu_eff_reverse_v11/180125_230555/0000/",			// 6 Ks HM7 pPb reverse
 	"NA",			// 7 Ks HM0 pPb merge
 	"NA",			// 8 Ks HM1 pPb merge
 	"NA",			// 9 Ks HM7 pPb merge
@@ -28,16 +28,16 @@ char const * fname[] = {
 	"NA",			// 25 Omega HM0 pPb merge
 	"NA",			// 26 Omega HM1 pPb merge
 	"NA",			// 27 Omega HM7 pPb merge
-	"../cumu/PAHighMultiplicity2/crab_PAHM2_Kscumu_eff_v10/180112_213619/0000/",			// 28 Ks HM2 pPb
-	"../cumu/PAHighMultiplicity3/crab_PAHM3_Kscumu_eff_v10/180112_213641/0000/",			// 29 Ks HM3 pPb
-	"../cumu/PAHighMultiplicity4/crab_PAHM4_Kscumu_eff_v10/180112_213659/0000/",			// 30 Ks HM4 pPb
-	"../cumu/PAHighMultiplicity5/crab_PAHM5_Kscumu_eff_v10/180112_213721/0000/",			// 31 Ks HM5 pPb
-	"../cumu/PAHighMultiplicity6/crab_PAHM6_Kscumu_eff_v10/180112_213751/0000/",			// 32 Ks HM6 pPb
-	"../cumu/PAHighMultiplicity2/crab_PAHM2_Kscumu_eff_reverse_v10/180112_214310/0000/",		// 33 Ks HM2 pPb reverse
-	"../cumu/PAHighMultiplicity3/crab_PAHM3_Kscumu_eff_reverse_v10/180112_214336/0000/",		// 34 Ks HM3 pPb reverse
-	"../cumu/PAHighMultiplicity4/crab_PAHM4_Kscumu_eff_reverse_v10/180112_214358/0000/",		// 35 Ks HM4 pPb reverse
-	"../cumu/PAHighMultiplicity5/crab_PAHM5_Kscumu_eff_reverse_v10/180112_214420/0000/",		// 36 Ks HM5 pPb reverse
-	"../cumu/PAHighMultiplicity6/crab_PAHM6_Kscumu_eff_reverse_v10/180112_214437/0000/",		// 37 Ks HM6 pPb reverse
+	"../cumu/PAHighMultiplicity2/crab_PAHM2_Kscumu_eff_v11/180125_225816/0000/",			// 28 Ks HM2 pPb
+	"../cumu/PAHighMultiplicity3/crab_PAHM3_Kscumu_eff_v11/180125_225842/0000/",			// 29 Ks HM3 pPb
+	"../cumu/PAHighMultiplicity4/crab_PAHM4_Kscumu_eff_v11/180125_225908/0000/",			// 30 Ks HM4 pPb
+	"../cumu/PAHighMultiplicity5/crab_PAHM5_Kscumu_eff_v11/180125_225935/0000/",			// 31 Ks HM5 pPb
+	"../cumu/PAHighMultiplicity6/crab_PAHM6_Kscumu_eff_v11/180125_230000/0000/",			// 32 Ks HM6 pPb
+	"../cumu/PAHighMultiplicity2/crab_PAHM2_Kscumu_eff_reverse_v11/180125_230342/0000/",		// 33 Ks HM2 pPb reverse
+	"../cumu/PAHighMultiplicity3/crab_PAHM3_Kscumu_eff_reverse_v11/180125_230409/0000/",		// 34 Ks HM3 pPb reverse
+	"../cumu/PAHighMultiplicity4/crab_PAHM4_Kscumu_eff_reverse_v11/180125_230436/0000/",		// 35 Ks HM4 pPb reverse
+	"../cumu/PAHighMultiplicity5/crab_PAHM5_Kscumu_eff_reverse_v11/180125_230502/0000/",		// 36 Ks HM5 pPb reverse
+	"../cumu/PAHighMultiplicity6/crab_PAHM6_Kscumu_eff_reverse_v11/180125_230528/0000/",		// 37 Ks HM6 pPb reverse
 	"../cumu/PAHighMultiplicity2/crab_PAHM2_Lambda_cumu_eff_v11/180119_195125/0000/",		// 38 Lambda HM2 pPb
 	"../cumu/PAHighMultiplicity3/crab_PAHM3_Lambda_cumu_eff_v11/180119_195149/0000/",		// 39 Lambda HM3 pPb
 	"../cumu/PAHighMultiplicity4/crab_PAHM4_Lambda_cumu_eff_v11/180119_195213/0000/",		// 40 Lambda HM4 pPb
@@ -60,6 +60,44 @@ char const * fname[] = {
 	"NA",			// 57 Ks HM123456 pPb reverse
 	"NA",			// 58 Lm HM123456 pPb
 	"NA",			// 59 Lm HM123456 pPb reverse
+	"../cumu/PAHighMultiplicity0/crab_PAHM0_KscumuSBPos_eff_v1/180128_050300/0000/",			// 60 Ks HM0 pPb SB Pos
+	"../cumu/PAHighMultiplicity1/crab_PAHM1_KscumuSBPos_eff_v1/180128_050314/0000/",			// 61 Ks HM1 pPb SB Pos
+	"../cumu/PAHighMultiplicity2/crab_PAHM2_KscumuSBPos_eff_v1/180128_050326/0000/",			// 62 Ks HM2 pPb SB Pos
+	"../cumu/PAHighMultiplicity3/crab_PAHM3_KscumuSBPos_eff_v1/180128_050338/0000/",			// 63 Ks HM3 pPb SB Pos
+	"../cumu/PAHighMultiplicity4/crab_PAHM4_KscumuSBPos_eff_v1/180128_050350/0000/",			// 64 Ks HM4 pPb SB Pos
+	"../cumu/PAHighMultiplicity5/crab_PAHM5_KscumuSBPos_eff_v1/180128_050402/0000/",			// 65 Ks HM5 pPb SB Pos
+	"../cumu/PAHighMultiplicity6/crab_PAHM6_KscumuSBPos_eff_v1/180128_050414/0000/",			// 66 Ks HM6 pPb SB Pos
+	"../cumu/PAHighMultiplicity7/crab_PAHM7_KscumuSBPos_eff_v1/180128_050426/0000/",			// 67 Ks HM7 pPb SB Pos
+	"",			// 68 Ks HM0 pPb SB Pos reverse
+	"",			// 69 Ks HM1 pPb SB Pos reverse
+	"",			// 70 Ks HM2 pPb SB Pos reverse
+	"",			// 71 Ks HM3 pPb SB Pos reverse
+	"",			// 72 Ks HM4 pPb SB Pos reverse
+	"",			// 73 Ks HM5 pPb SB Pos reverse
+	"",			// 74 Ks HM6 pPb SB Pos reverse
+	"",			// 75 Ks HM7 pPb SB Pos reverse
+	"NA",			// 76 Ks HM0 pPb SB Pos merge
+	"NA",			// 77 Ks HM123456 pPb SB Pos merge
+	"NA",			// 78 Ks HM7 pPb SB Pos merge
+	"",			// 79 Lm HM0 pPb SB Pos
+	"",			// 80 Lm HM1 pPb SB Pos
+	"",			// 81 Lm HM2 pPb SB Pos
+	"",			// 82 Lm HM3 pPb SB Pos
+	"",			// 83 Lm HM4 pPb SB Pos
+	"",			// 84 Lm HM5 pPb SB Pos
+	"",			// 85 Lm HM6 pPb SB Pos
+	"",			// 86 Lm HM7 pPb SB Pos
+	"",			// 87 Lm HM0 pPb SB Pos reverse
+	"",			// 88 Lm HM1 pPb SB Pos reverse
+	"",			// 89 Lm HM2 pPb SB Pos reverse
+	"",			// 90 Lm HM3 pPb SB Pos reverse
+	"",			// 91 Lm HM4 pPb SB Pos reverse
+	"",			// 92 Lm HM5 pPb SB Pos reverse
+	"",			// 93 Lm HM6 pPb SB Pos reverse
+	"",			// 94 Lm HM7 pPb SB Pos reverse
+	"NA",			// 95 Lm HM0 pPb SB Pos merge
+	"NA",			// 96 Lm HM123456 pPb SB Pos merge
+	"NA",			// 97 Lm HM7 pPb SB Pos merge
 };
 
 char const * ftxt[] = {
@@ -123,6 +161,44 @@ char const * ftxt[] = {
 	"txt/PAHM123456_Ks_pPb_reverse/",	// 57
 	"txt/PAHM123456_Lm_pPb/",		// 58
 	"txt/PAHM123456_Lm_pPb_reverse/",	// 59
+	"txt/PAHM0_KsSBPos_pPb/",		// 60
+	"txt/PAHM1_KsSBPos_pPb/",		// 61
+	"txt/PAHM2_KsSBPos_pPb/",		// 62
+	"txt/PAHM3_KsSBPos_pPb/",		// 63
+	"txt/PAHM4_KsSBPos_pPb/",		// 64
+	"txt/PAHM5_KsSBPos_pPb/",		// 65
+	"txt/PAHM6_KsSBPos_pPb/",		// 66
+	"txt/PAHM7_KsSBPos_pPb/",		// 67
+	"txt/PAHM0_KsSBPos_pPb_reverse/",	// 68
+	"txt/PAHM1_KsSBPos_pPb_reverse/",	// 69
+	"txt/PAHM2_KsSBPos_pPb_reverse/",	// 70
+	"txt/PAHM3_KsSBPos_pPb_reverse/",	// 71
+	"txt/PAHM4_KsSBPos_pPb_reverse/",	// 72
+	"txt/PAHM5_KsSBPos_pPb_reverse/",	// 73
+	"txt/PAHM6_KsSBPos_pPb_reverse/",	// 74
+	"txt/PAHM7_KsSBPos_pPb_reverse/",	// 75
+	"txt/PAHM0_KsSBPos_pPb_merge/",		// 76
+	"txt/PAHM123456_KsSBPos_pPb_merge/",	// 77
+	"txt/PAHM7_KsSBPos_pPb_merge/",		// 78
+	"txt/PAHM0_LmSBPos_pPb/",		// 79
+	"txt/PAHM1_LmSBPos_pPb/",		// 80
+	"txt/PAHM2_LmSBPos_pPb/",		// 81
+	"txt/PAHM3_LmSBPos_pPb/",		// 82
+	"txt/PAHM4_LmSBPos_pPb/",		// 83
+	"txt/PAHM5_LmSBPos_pPb/",		// 84
+	"txt/PAHM6_LmSBPos_pPb/",		// 85
+	"txt/PAHM7_LmSBPos_pPb/",		// 86
+	"txt/PAHM0_LmSBPos_pPb_reverse/",	// 87
+	"txt/PAHM1_LmSBPos_pPb_reverse/",	// 88
+	"txt/PAHM2_LmSBPos_pPb_reverse/",	// 89
+	"txt/PAHM3_LmSBPos_pPb_reverse/",	// 90
+	"txt/PAHM4_LmSBPos_pPb_reverse/",	// 91
+	"txt/PAHM5_LmSBPos_pPb_reverse/",	// 92
+	"txt/PAHM6_LmSBPos_pPb_reverse/",	// 93
+	"txt/PAHM7_LmSBPos_pPb_reverse/",	// 94
+	"txt/PAHM0_LmSBPos_pPb_merge/",		// 95
+	"txt/PAHM123456_LmSBPos_pPb_merge/",	// 96
+	"txt/PAHM7_LmSBPos_pPb_merge/",		// 97
 };
 
 const int Pid_Non	= 0;
@@ -199,6 +275,44 @@ auto PID = std::vector<int>{
 	Pid_PA_Ks,	// 57
 	Pid_PA_Lambda,	// 58
 	Pid_PA_Lambda,	// 59
+	Pid_PA_Ks,		// 60
+	Pid_PA_Ks,		// 61
+	Pid_PA_Ks,		// 62
+	Pid_PA_Ks,		// 63
+	Pid_PA_Ks,		// 64
+	Pid_PA_Ks,		// 65
+	Pid_PA_Ks,		// 66
+	Pid_PA_Ks,		// 67
+	Pid_PA_Ks,		// 68
+	Pid_PA_Ks,		// 69
+	Pid_PA_Ks,		// 70
+	Pid_PA_Ks,		// 71
+	Pid_PA_Ks,		// 72
+	Pid_PA_Ks,		// 73
+	Pid_PA_Ks,		// 74
+	Pid_PA_Ks,		// 75
+	Pid_PA_Ks,		// 76
+	Pid_PA_Ks,		// 77
+	Pid_PA_Ks,		// 78
+	Pid_PA_Lambda,		// 79
+	Pid_PA_Lambda,		// 80
+	Pid_PA_Lambda,		// 81
+	Pid_PA_Lambda,		// 82
+	Pid_PA_Lambda,		// 83
+	Pid_PA_Lambda,		// 84
+	Pid_PA_Lambda,		// 85
+	Pid_PA_Lambda,		// 86
+	Pid_PA_Lambda,		// 87
+	Pid_PA_Lambda,		// 88
+	Pid_PA_Lambda,		// 89
+	Pid_PA_Lambda,		// 90
+	Pid_PA_Lambda,		// 91
+	Pid_PA_Lambda,		// 92
+	Pid_PA_Lambda,		// 93
+	Pid_PA_Lambda,		// 94
+	Pid_PA_Lambda,		// 95
+	Pid_PA_Lambda,		// 96
+	Pid_PA_Lambda,		// 97
 };
 
 #include <TChain.h>
